1d8ab227291d09d62975fcaa356b7454565005a6
Rafael30s <rafael.santos@bandtec.com.br>
2020-07-08 19:47:31 -0300

    JS assíncrono

---
+++ javascript/main.js
@@ -0,0 +1,50 @@
+//Aula 1 - Requisições AJAX
+
+//var xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://api.github.com/users/Rafael30s');
+// xhr.send(null);
+
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState === 4){
+//         console.log(JSON.parse(xhr.responseText));
+//     }
+// }
+
+// Aula 2- Promiss
+
+//var minhaPromise = function() {
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://api.github.com/users/Rafael30s');
+//         xhr.send(null);
+
+//         xhr.onreadystatechange = function() {
+//             if(xhr.readyState === 4){
+//                 if(xhr.status === 200){
+//                     resolve(JSON.parse(xhr.responseText));
+//                 } else{
+//                     reject('Erro na requisição');
+//                 }
+//             }
+//         }
+//     })
+// }
+
+// minhaPromise()
+// .then(function(response){
+//     console.log(response);
+// })
+// .catch(function(error){
+//     console.warn(error);
+// });
+
+
+//Aula3 -Utilizando Axios
+axios.get('https://api.github.com/users/Rafael30s')
+.then(function(response){
+    console.log(response.data.avatar_url);
+})
+.catch(function(error){
+    console.warn(error);
+});
