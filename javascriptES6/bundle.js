186ceae22f0b07c2594b34830673e891a393e675
Rafael30s <rafael.santos@bandtec.com.br>
2020-07-12 23:12:14 -0300

    Classes

--- javascriptES6/bundle.js
+++ javascriptES6/bundle.js
@@ -1,8 +1,38 @@
-alert('teste');
-
-class Teste {
-  metodo() {}
-
-  outro() {}
+class Matematica {
+  static soma(a, b) {
+    return a + b;
+  }
 
 }
+
+console.log(Matematica.soma(1, 2));
+
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = 'Rafael';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaLista.add('Novo todo');
+};
--- javascriptES6/main.js
+++ javascriptES6/main.js
@@ -1,10 +1,34 @@
-alert('teste');
-
-class Teste {
-    metodo(){
-
+class Matematica{
+    static soma(a,b){
+        return a+b;
     }
-    outro(){
-        
+}
+
+console.log(Matematica.soma(1,2));
+
+class List {
+    constructor(){
+        this.data=[];
     }
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List{
+    constructor(){
+        super();
+        this.usuario = 'Rafael';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
 }
