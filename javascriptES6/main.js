9479c549bbd3d64cf567ac61143eac820d8a1177
Rafael30s <rafael.santos@bandtec.com.br>
2020-07-14 21:33:06 -0300

    Operações em array

--- javascriptES6/main.js
+++ javascriptES6/main.js
@@ -37,16 +37,43 @@
 
 // Aula 2- Const-Let
 
-const usuario ={nome: 'Rafael'}; // não pode ter seu valor reatribuido
+// const usuario ={nome: 'Rafael'}; // não pode ter seu valor reatribuido
 
-usuario.nome = 'Cleiton'; // mutação em const é possivel
+// usuario.nome = 'Cleiton'; // mutação em const é possivel
 
-function teste(x){
-    let y=2;
+// function teste(x){
+//     let y=2;
 
-    if(x>5){
-        console.log(x,y);
-    }
-}
+//     if(x>5){
+//         console.log(x,y);
+//     }
+// }
 
-teste(10);
+// teste(10);
+
+// Aula 3- Operações em array
+
+const arr =[1, 3, 4, 5, 8,9];
+const newArr= arr.map(function(item, index){
+   return item+ index; 
+});
+
+console.log(newArr);
+
+const sum= arr.reduce(function(total,next){
+    return total+ next;
+})
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 ===0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item ===4;
+});
+
+console.log(find);
